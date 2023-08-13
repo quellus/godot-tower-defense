9d51a51d929fdab220c7e207642bf1afca0dbe41
Quellus <quellus7@gmail.com>
2023-08-13 01:34:39 -0600

    A little cleanup and reorganization

--- Enemy/EnemyPathFollow.gd
+++ Enemy/EnemyPathFollow.gd
@@ -1,10 +1,6 @@
 extends PathFollow
 
 var movement_speed: float = 0.1
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 	
 func _physics_process(delta) -> void:
 	var parent = get_parent()
--- Enemy/EnemySpawner.gd
+++ Enemy/EnemySpawner.gd
@@ -2,11 +2,8 @@ extends Spatial
 var coords: Array = []
 onready var path = get_node("/root/L_Main/Path")
 onready var timer = get_node("Timer")
-onready var enemy = load("res://EnemyPathFollow.tscn")
+onready var enemy = load("res://Enemy/EnemyPathFollow.tscn")
 onready var enemies = get_node("/root/L_Main/Path")
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 func _physics_process(delta) -> void:
 	var first_point: Vector3 = path.curve.get_point_position(0)
@@ -17,4 +14,3 @@ func _on_Timer_timeout():
 	var first_point: Vector3 = path.curve.get_point_position(0)
 	enemies.add_child(enemyInstance)
 	enemyInstance.global_translate(first_point)
-	pass # Replace with function body.
