725c4a6f363c3b62133575ec84ae282dee7b74b1
quellus <quellus7@gmail.com>
2024-05-22 16:46:14 -0600

    Rework new player controller to rotate character when moved

---
+++ Player/PlayerInteraction.gd
@@ -0,0 +1,20 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	#if event.is_action_just_pressed("fire"):
+		#var bodies = get_overlapping_bodies()
+		#if bodies =
+		#if bodies.size() > 0:
+			#var target = Global.get_closest(bodies, global_position)
+	pass
+
--- Player/PlayerTopDown.gd
+++ Player/PlayerTopDown.gd
@@ -2,7 +2,6 @@ extends CharacterBody3D
 
 
 const SPEED = 10.0
-const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -13,17 +12,12 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	if not input_dir == Vector2(0,0):
+		velocity.x = (transform.basis * Vector3.FORWARD * SPEED).x
+		velocity.z = (transform.basis * Vector3.FORWARD * SPEED).z
+		#rotation.y = lerp_angle(Vector2(0,-1).angle_to(input_dir) * -1, rotation.y, 0.7)
+		rotation.y = lerp_angle(input_dir.angle_to(Vector2(0,-1)), rotation.y, 0.7)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
